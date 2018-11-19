9909586023adc62596ebcb0c80e6a26fcb3244d3
winget82 <nflesher82@yahoo.com>
2018-11-18 22:21:44 -0600

    11/18/18 update

--- Battleships.java
+++ Battleships.java
@@ -17,6 +17,16 @@ public class Battleships {
 
         //print out the updated sea grid
         Sea(sea);
+        DeployComputerShips(sea);
+        boolean gameOn = true;
+        Sea(sea);
+
+        while (gameOn == true){
+            PlayerAttack(sea); //ISSUE HERE
+            ComputerAttack(sea);
+            Sea(sea);
+        }
+
     }
 
     public static char[][] Sea (char sea[][]) {
@@ -29,6 +39,10 @@ public class Battleships {
                     System.out.print(" ");//goes across row printing spaces where there is no character
                 } else if (sea[row][col] == '1') {
                     System.out.print('@');//print out player ship positions as '@' on the sea grid
+                } else if (sea[row][col] == '2') {
+                    System.out.print('X');//print out computer ship positions as 'X' on the sea grid
+                } else {
+                    System.out.print(sea[row][col]);
                 }
             }
             System.out.println("|" + row);//prints right side border and row number
@@ -70,10 +84,48 @@ public class Battleships {
         input.close();
         return sea;
     }
-    //need to randomly deploy computer's ships
 
-    //set up a while loop for the game for the battle
+    public static char[][] DeployComputerShips(char sea[][]) {
+        for (int computerShip = 1; computerShip < 6; computerShip++) {
+            int x = (int)(Math.random() * 10);
+            int y = (int)(Math.random() * 10);
+            while (sea[x][y] != 0){
+                x = (int)(Math.random() * 10);
+                y = (int)(Math.random() * 10);
+            }
+            sea[x][y] = '2';
+            System.out.println("Computer ship " + computerShip + " is deployed.");
+        }
+        System.out.println();
+        return sea;
+    }
+
+    public static char[][] PlayerAttack(char sea[][]) {
+        Scanner attackInput = new Scanner(System.in);
+
+        System.out.print("Enter X coordinate to attack!");
+        int x = attackInput.nextInt();
+        while (x > 9 || x < 0) {
+            System.out.println("Cannot attack outside of the sea grid.  Please choose a x-value between 0-9.");
+            x = attackInput.nextInt();
+        }
+        System.out.print("Enter Y coordinate to attack!");
+        int y = attackInput.nextInt();
+        while (y > 9 || y < 0) {
+            System.out.println("Cannot attack outside of the sea grid.  Please choose a y-value between 0-9.");
+            y = attackInput.nextInt();
+        }
+        sea[x][y] = '#';
+        attackInput.close();
+        return sea;
+    }
+
+    public static char[][] ComputerAttack(char sea[][]) {
+        int x = (int)(Math.random() * 10);
+        int y = (int)(Math.random() * 10);
+        sea[x][y] = '*';
+        return sea;
+    }
 
     //set up the the end of game
-
 }
